64f156afd6416b8fb9039fa158952ef8dd950a60
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-02-13 14:41:39 +0200

    Adding YoutubeView to UI.

--- app/src/main/java/com/gmail/alexander/vieweryoutube/YoutubeActivity.java
+++ app/src/main/java/com/gmail/alexander/vieweryoutube/YoutubeActivity.java
@@ -1,17 +1,26 @@
 package com.gmail.alexander.vieweryoutube;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.view.ViewGroup;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(constraintLayout);
+
+    YouTubePlayerView youTubePlayer= new YouTubePlayerView(this);
+    youTubePlayer.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+    constraintLayout.addView(youTubePlayer);
     }
 
     @Override
